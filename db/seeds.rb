e1175903bc681203b4cbdb2b7b5d9bf9a4317c6d
Andy Lau <lau.andy.lau@gmail.com>
2018-10-18 21:29:29 +1100

    heroku push with fixed css and fonts

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,41 +39,41 @@ puts "#{Restaurant.all.length} restaurants"
 
 Review.destroy_all
 
-r1 = Review.create content: "It was really good.", rating: 3, user_id: u1.id, restaurant_id: re1.id, image: "http://placekitten.com/250/250"
+r1 = Review.create content: "It was really good. Wish I could come again.", rating: 3, user_id: u1.id, restaurant_id: re1.id, image: "/assets/food1.jpeg"
 
-r2 = Review.create content: "Amazing! Had an excellent time", rating: 4, user_id: u3.id, restaurant_id: re2.id, image: "http://placekitten.com/250/250"
+r2 = Review.create content: "Amazing! Had an excellent time", rating: 4, user_id: u3.id, restaurant_id: re2.id, image: "/assets/food2.jpg"
 
-r3 = Review.create content: "It was really bad!! Never coming again!", rating: 1, user_id: u2.id, restaurant_id: re2.id, image: "http://placekitten.com/250/250"
+r3 = Review.create content: "It was really bad!! Never coming again!", rating: 1, user_id: u2.id, restaurant_id: re2.id, image: "/assets/food3.JPG"
 
-r4 = Review.create content: "It was really good!! Always coming again!", rating: 5, user_id: u5.id, restaurant_id: re3.id, image: "http://placekitten.com/250/250"
+r4 = Review.create content: "It was really good!! Service was very good.", rating: 5, user_id: u5.id, restaurant_id: re3.id, image: "/assets/food4.jpg"
 
-r5 = Review.create content: "WOW SO good! Always coming again!", rating: 3, user_id: u2.id, restaurant_id: re6.id, image: "http://placekitten.com/250/250"
+r5 = Review.create content: "WOW SO good! Always coming again!", rating: 3, user_id: u2.id, restaurant_id: re6.id, image: "/assets/food5.jpg"
 
-r6 = Review.create content: "It was really good!! Always coming again!", rating: 5, user_id: u3.id, restaurant_id: re1.id, image: "http://placekitten.com/250/250"
+r6 = Review.create content: "It was really good!! Always coming again!", rating: 5, user_id: u3.id, restaurant_id: re1.id, image: "/assets/food6.jpeg"
 
-r7 = Review.create content: "It was really good!! Always so GOOD", rating: 2, user_id: u4.id, restaurant_id: re3.id, image: "http://placekitten.com/250/250"
+r7 = Review.create content: "It was really good!! Always so GOOD", rating: 2, user_id: u4.id, restaurant_id: re3.id, image: "/assets/food7.jpg"
 
-r8 = Review.create content: "It was really good!! Always coming again!", rating: 5, user_id: u2.id, restaurant_id: re3.id, image: "http://placekitten.com/250/250"
+r8 = Review.create content: "It was really good!! Always coming again!", rating: 5, user_id: u2.id, restaurant_id: re3.id, image: "/assets/food8.jpg"
 
-r9 = Review.create content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Always coming again!", rating: 5, user_id: u6.id, restaurant_id: re4.id, image: "http://placekitten.com/250/250"
+r9 = Review.create content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", rating: 5, user_id: u6.id, restaurant_id: re4.id, image: "/assets/food9.jpg"
 
-r10 = Review.create content: "It was really good!! Always coming again!", rating: 5, user_id: u3.id, restaurant_id: re4.id, image: "http://placekitten.com/250/250"
+r10 = Review.create content: "It was really good!! Always coming again!", rating: 3, user_id: u3.id, restaurant_id: re4.id, image: "/assets/food11.jpg"
 
-r11 = Review.create content: "It was really good!! Always coming again!", rating: 5, user_id: u4.id, restaurant_id: re5.id, image: "http://placekitten.com/250/250"
+r11 = Review.create content: "It was really good!! Always coming again!", rating: 5, user_id: u4.id, restaurant_id: re5.id, image: "/assets/food1.jpg"
 
-r12 = Review.create content: "It was really good!! Always coming again!", rating: 5, user_id: u4.id, restaurant_id: re2.id, image: "http://placekitten.com/250/250"
+r12 = Review.create content: "OMG! Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", rating: 5, user_id: u4.id, restaurant_id: re2.id, image: "/assets/food12.jpg"
 
-r13 = Review.create content: "It was really good!! Always coming again!", rating: 5, user_id: u6.id, restaurant_id: re1.id, image: "http://placekitten.com/250/250"
+r13 = Review.create content: "It was really good!! Always coming again!", rating: 5, user_id: u6.id, restaurant_id: re1.id, image: "/assets/food3.JPG"
 
-r14 = Review.create content: "It was really good!! Always coming again!", rating: 5, user_id: u3.id, restaurant_id: re8.id, image: "http://placekitten.com/250/250"
+r14 = Review.create content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", rating: 5, user_id: u3.id, restaurant_id: re8.id, image: "/assets/food5.jpg"
 
-r15 = Review.create content: "It was really good!! Always coming again!", rating: 3, user_id: u5.id, restaurant_id: re8.id, image: "http://placekitten.com/250/250"
+r15 = Review.create content: "Was very expensive but worth the price. ", rating: 3, user_id: u5.id, restaurant_id: re8.id, image: "/assets/food12.jpg"
 
-r16 = Review.create content: "It was really good!! Always coming again!", rating: 2, user_id: u4.id, restaurant_id: re1.id, image: "http://placekitten.com/250/250"
+r16 = Review.create content: "Service was incredibly bad. Waiter spat in my food.", rating: 2, user_id: u4.id, restaurant_id: re1.id, image: "/assets/food11.jpg"
 
-r17 = Review.create content: "It was really good!! Always coming again!", rating: 5, user_id: u1.id, restaurant_id: re5.id, image: "http://placekitten.com/250/250"
+r17 = Review.create content: "The dessert was so good. Always a pleasure.", rating: 5, user_id: u1.id, restaurant_id: re5.id, image: "/assets/food7.jpg"
 
-r18 = Review.create content: "Amazing! Had an excellent time", rating: 1, user_id: u3.id, restaurant_id: re7.id, image: "http://placekitten.com/250/250"
+r18 = Review.create content: "Amazing! Had an excellent time", rating: 1, user_id: u3.id, restaurant_id: re7.id, image: "/assets/food6.jpeg"
 
 puts "#{Review.all.length} reviews"
 
@@ -81,3 +81,4 @@ r18.liked_by << u1 << u2 << u3
 r14.liked_by << u4 << u2 << u6
 r12.liked_by << u1 << u6 << u4
 r10.liked_by << u4 << u5 << u4
+r1.liked_by << u2 << u6 << u3 << u4
